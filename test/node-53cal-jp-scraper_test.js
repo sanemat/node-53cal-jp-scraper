db00b7508e039295b72a7b49ef3c92250f8d730d
sanemat <o.gata.ken@gmail.com>
2014-06-04 19:57:14 +0900

    Use assert instead of should

--- test/node-53cal-jp-scraper_test.js
+++ test/node-53cal-jp-scraper_test.js
@@ -1,12 +1,12 @@
 'use strict';
 
 var node53calJpScraper = require('../lib/node-53cal-jp-scraper.js');
-var assert = require('should');
+var assert = require('assert');
 
 describe('node53calJpScraper', function () {
 
   it('should be awesome', function () {
-    node53calJpScraper.awesome().should.equal('awesome');
+    assert.equal(node53calJpScraper.awesome(), 'awesome');
   });
 
 });
