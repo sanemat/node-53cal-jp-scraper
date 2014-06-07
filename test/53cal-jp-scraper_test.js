1055c0826cf234d54bc6cb5526da81927120433b
sanemat <o.gata.ken@gmail.com>
2014-06-08 01:59:34 +0900

    Adjust example

--- test/53cal-jp-scraper_test.js
+++ test/53cal-jp-scraper_test.js
@@ -1,11 +1,12 @@
 'use strict';
 
-var gomiCalJpScraper = require('../lib/53cal-jp-scraper.js');
+var GomiCalJp = require('../lib/53cal-jp-scraper.js');
 var assert = require('power-assert');
 
 describe('gomiCalJpScraper', function () {
   it('should be categories', function(done){
-    gomiCalJpScraper.categories({city: '1130104', area: '1130104154'}, function(data){
+    var scraper = GomiCalJp({ city: '1130104', area: '1130104154' });
+    scraper.categories(function(data){
       assert.deepEqual(data.meta, {city: '1130104', area: '1130104154'});
       done();
     });
