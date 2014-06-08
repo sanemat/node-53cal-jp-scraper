cf4f6b926fa566e6bc22f4141d302ceff9048cef
sanemat <o.gata.ken@gmail.com>
2014-06-09 03:14:56 +0900

    Use error

--- test/53cal-jp-scraper_test.js
+++ test/53cal-jp-scraper_test.js
@@ -16,7 +16,7 @@ describe('gomiCalJpScraper', function () {
     nock('http://www.53cal.jp')
       .get('/areacalendar/?city=1130104&area=1130104154')
       .replyWithFile(200, __dirname + '/53caljp-minamioi-20140531.html');
-    scraper.categories(function(data){
+    scraper.categories(function(err, data){
       assert.deepEqual(data.meta, {city: '1130104', area: '1130104154'});
       assert.deepEqual(data.result, ['燃やすごみ', '陶器・ガラス・金属ごみ','資源']);
       done();
@@ -27,7 +27,7 @@ describe('gomiCalJpScraper', function () {
     nock('http://www.53cal.jp')
       .get('/areacalendar/?city=1130104&area=1130104154&yy=2014&mm=6')
       .replyWithFile(200, __dirname + '/53caljp-minamioi-20140601.html');
-    scraper.dateAndCategoryInMonth(2014, 6, function(data){
+    scraper.dateAndCategoryInMonth(2014, 6, function(err, data){
       assert.deepEqual(data.meta, {city: '1130104', area: '1130104154', year: 2014, month: 6});
       assert.equal(data.result.length, 30);
       assert.deepEqual(data.result[0], {'2014-06-01': null});
