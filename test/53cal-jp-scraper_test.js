f46d45c08f8057a0867f024d4870f63843e36a78
sanemat <o.gata.ken@gmail.com>
2014-06-09 22:34:18 +0900

    Add cityName, areaName

--- test/53cal-jp-scraper_test.js
+++ test/53cal-jp-scraper_test.js
@@ -17,7 +17,7 @@ describe('gomiCalJpScraper', function () {
       .get('/areacalendar/?city=1130104&area=1130104154')
       .replyWithFile(200, __dirname + '/53caljp-minamioi-20140531.html');
     scraper.categories(function(err, data){
-      assert.deepEqual(data.meta, {city: '1130104', area: '1130104154'});
+      assert.deepEqual(data.meta, {city: '1130104', area: '1130104154', cityName: '東京都品川区', areaName: '南大井6丁目18番地(大森住宅）以外'});
       assert.deepEqual(data.result, ['燃やすごみ', '陶器・ガラス・金属ごみ','資源']);
       done();
     });
@@ -28,7 +28,7 @@ describe('gomiCalJpScraper', function () {
       .get('/areacalendar/?city=1130104&area=1130104154&yy=2014&mm=6')
       .replyWithFile(200, __dirname + '/53caljp-minamioi-20140601.html');
     scraper.dateAndCategoryInMonth(2014, 6, function(err, data){
-      assert.deepEqual(data.meta, {city: '1130104', area: '1130104154', year: 2014, month: 6});
+      assert.deepEqual(data.meta, {city: '1130104', area: '1130104154', year: 2014, month: 6, cityName: '東京都品川区',areaName: '南大井6丁目18番地(大森住宅）以外'});
       assert.equal(data.result.length, 30);
       assert.deepEqual(data.result[0], {'2014-06-01': null});
       assert.deepEqual(data.result[1], {'2014-06-02': '陶器・ガラス・金属ごみ'});
